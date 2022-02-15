e8d179e08b836e58481314b39fb668d1e448256b
echevalier <estebanchevalier@gmail.com>
2022-02-15 20:14:47 -0300

    Improve POST productsRouters, Add CartContainer.

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 import 'dotenv/config'
 import express from 'express';
 import { Contenedor } from './Contenedor.js'
+import { CartContainer } from './CartContainer.js'
 
 const app = express()
-const productosRouters = express.Router()
-const carritoRouters = express.Router()
+const productsRouters = express.Router()
+const cartsRouters = express.Router()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/bootstrap', express.static('node_modules/bootstrap/dist'))
-app.use('/api/productos', productosRouters)
-app.use('/api/carrito', carritoRouters)
+app.use('/api/productos', productsRouters)
+app.use('/api/carrito', cartsRouters)
 
 const PORT = process.env.PORT || process.env.PORT_DEV
 
 app.set('views', './views'); // especifica el directorio de vistas
 app.set('view engine', 'ejs'); // registra el motor de plantillas
 
-const server = app.listen(PORT, () => {
-    console.log(`Servidor http escuchando en el puerto ${server.address().port}
+const server = app.listen(PORT, (err) => {
+    if (err) {
+        console.log("Error while starting server");
+    } else {
+        console.log(`Servidor http escuchando en el puerto ${server.address().port}
                  Open link to http://127.0.0.1:${server.address().port}`)
+    }
 })
 
 console.log(server.address())
@@ -27,66 +32,82 @@ console.log(server.address())
 server.on("error", error => console.log(`Error en servidor ${error}`))
 
 
+/**
+ * Container instance 
+ */
 const contenedor = new Contenedor('productos.txt')
-const productos = await contenedor.getAll()
-//productos.length = 0
-
-const fakeApi = () => productos
-const fakeApiOne = (id) => productos.filter(prod => prod.id == id) 
+const products = await contenedor.getAll()
+//products.length = 0
 
 /**
- *  Productos EndPoint - Users and Admin
+ *  Carts instance
+ */
+const cartsContainer = new CartContainer('carritos.txt')
+const carts = await cartsContainer.getAll()
+console.log(carts)
+
+
+
+const fakeApi = () => products
+const fakeApiOne = (id) => products.filter(prod => prod.id == id)
+
+/**
+ *  products EndPoint - Users and Admin
  */
 
-productosRouters.get('/:id?', (req, res) => {
+productsRouters.get('/:id?', (req, res) => {
     if (req.params.id) {
-        console.log(`GET => id: ${req.params.id} -- productosRouters`);
-        res.render('page/productList', {productos: fakeApiOne(req.params.id), isEmpty: fakeApiOne(req.params.id).length? false:true})
+        console.log(`GET => id: ${req.params.id} -- productsRouters`);
+        res.render('page/productList', { products: fakeApiOne(req.params.id), isEmpty: fakeApiOne(req.params.id).length ? false : true })
     } else {
-        console.log(`GET ALL -- productosRouters`);
-        res.render('page/productList', {productos: fakeApi(), isEmpty: fakeApi().length? false:true})
+        console.log(`GET ALL -- productsRouters`);
+        res.render('page/productList', { products: fakeApi(), isEmpty: fakeApi().length ? false : true })
     }
 })
 
 /**
- *  Productos EndPoints - ADMIN ONLY
+ *  products EndPoints - ADMIN ONLY
  */
 
-productosRouters.post('/', (req, res) => {
-    console.log(`POST -- productosRouters`);
+
+productsRouters.post('/', (req, res) => {
+    console.log(`POST -- productsRouters`);
     let prod = req.body
-    if ( Object.keys(prod).length !== 0 && prod.title !== '' && prod.price !== '' && prod.thumbnail !== '') {
-        const max = productos.reduce((a,b) => a.id > b.id ? a:b, {id: 0} )
+    
+    if (Object.keys(prod).length !== 0 && !Object.values(prod).includes('')) {
+        const max = products.reduce((a, b) => a.id > b.id ? a : b, { id: 0 })
         prod.id = max.id + 1
-        productos.push(prod)
-        contenedor.save(prod) 
+        prod.timestamp = Date.now()
+        products.push(prod)
+        contenedor.save(prod)
     }
-    res.json(prod)
-    //res.render('page/form')
+
+    //res.json(prod)
+    res.render('page/form')
 })
 
-productosRouters.put('/:id', (req, res) => {
-    console.log(`PUT => id: ${req.params.id} -- productosRouters`);
+productsRouters.put('/:id', (req, res) => {
+    console.log(`PUT => id: ${req.params.id} -- productsRouters`);
     let prod = req.body
     let id = req.params.id
-    let index = productos.findIndex(prod => prod.id == id)
-    if ( index >= 0) {
+    let index = products.findIndex(prod => prod.id == id)
+    if (index >= 0) {
         prod.id = id
-        productos[index] = prod
+        products[index] = prod
         contenedor.updateById(id, prod)
     }
-    res.json(index >= 0 ? {id: id}: {error: 'Producto no encontrado.'})
+    res.json(index >= 0 ? { id: id } : { error: 'Producto no encontrado.' })
 })
 
-productosRouters.delete('/:id', (req, res) => {
-    console.log(`DELETE => id: ${req.params.id} -- productosRouters`);
+productsRouters.delete('/:id', (req, res) => {
+    console.log(`DELETE => id: ${req.params.id} -- productsRouters`);
     let id = req.params.id
-    let index = productos.findIndex(prod => prod.id == id)
-    if (index >= 0 ) { 
-        productos.splice(index, 1)
+    let index = products.findIndex(prod => prod.id == id)
+    if (index >= 0) {
+        products.splice(index, 1)
         contenedor.deleteById(id)
     }
-    res.json(index >= 0 ? {id: id}: {error: 'Producto no encontrado.'})
+    res.json(index >= 0 ? { id: id } : { error: 'Producto no encontrado.' })
 })
 
 
@@ -94,27 +115,33 @@ productosRouters.delete('/:id', (req, res) => {
  *  Carrito EndPoints
  */
 
-carritoRouters.post('', (req, res) => {
-    res.json({status: "POST CREATE CART RETURN ID", id: 12})
+cartsRouters.post('', (req, res) => {
+    res.json({ status: "POST CREATE CART RETURN ID", id: 12 })
 })
 
-carritoRouters.delete('/:id', (req, res) => {
-    res.json({status: `DELETE CART WITH ID: ${req.params.id}` })
+cartsRouters.delete('/:id', (req, res) => {
+    res.json({ status: `DELETE CART WITH ID: ${req.params.id}` })
 })
 
-carritoRouters.get('/:id/productos', (req, res) => {
-    res.json({status: "GET PRODUCTOS FROM CART", id: req.params.id})
+cartsRouters.get('/:id/products', (req, res) => {
+    res.json({ status: "GET products FROM CART", id: req.params.id })
 })
 
-carritoRouters.post('/:id/productos', (req, res) => {
-    res.json({status: "POST PRODUCTOS TO CART", id: req.params.id})
+cartsRouters.post('/:id/products', (req, res) => {
+    res.json({ status: "POST products TO CART", id: req.params.id })
 })
 
-carritoRouters.delete('/:id/productos/:id_prod', (req, res) => {
-    res.json({status: `DELETE PRODUCTO IDPROD: ${req.params.id_prod}  FROM CART ID: ${req.params.id_prod}`, id: req.params.id, id_prod: req.params.id})
+cartsRouters.delete('/:id/products/:id_prod', (req, res) => {
+    res.json({ status: `DELETE PRODUCTO IDPROD: ${req.params.id_prod}  FROM CART ID: ${req.params.id_prod}`, id: req.params.id, id_prod: req.params.id })
 })
 
 
+/**
+ * Undefined endpoint
+ */
+app.all('*', (req, res) => {
+    res.json({ error : -2, descripcion: `ruta ${req.url} método ${req.method} no implementada`})
+})
 
 
 
